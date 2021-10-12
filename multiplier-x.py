70f1b452814e47bb580e418844c2ead936fafe81
Matthew <99067338@mydavinci.nl>
2021-10-12 09:42:47 +0200

    added multiplier-x

---
+++ multiplier-x.py
@@ -0,0 +1,21 @@
+def printGetal(getal):
+    print (getal) 
+
+
+
+getal = int(input('Welke tafel wil je?: '))
+
+printGetal(getal * 1)
+printGetal(getal * 2)
+printGetal(getal * 3)
+printGetal(getal * 4)
+printGetal(getal * 5)
+printGetal(getal * 6)
+printGetal(getal * 7)
+printGetal(getal * 8)
+printGetal(getal * 9)
+printGetal(getal * 10)
+
+
+
+
